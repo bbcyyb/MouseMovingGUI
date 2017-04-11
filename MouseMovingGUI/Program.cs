78a41a3061a8093486e9732a1e68fcedc22c8bfb
Kevin.Y.Yu <Kevin.Y.Yu@newegg.com>
2017-04-10 17:40:52 -0700

    Add mutex to ensure only one process running

--- MouseMovingGUI/Program.cs
+++ MouseMovingGUI/Program.cs
@@ -8,15 +8,34 @@ namespace MouseMovingGUI
 {
     static class Program
     {
+
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Form1());
+        //}
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            bool isAppRunning = false;
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, System.Diagnostics.Process.GetCurrentProcess().ProcessName, out isAppRunning);
+            if (!isAppRunning)
+            {
+                MessageBox.Show("本程序已经在运行了，请不要重复运行！");
+                Environment.Exit(1);
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
         }
     }
 }
