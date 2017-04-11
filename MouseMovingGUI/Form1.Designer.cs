9baf682b46e4b1bd5eee0f7d0a87c0b762f65f1e
Kevin.Y.Yu <Kevin.Y.Yu@newegg.com>
2017-04-10 17:33:34 -0700

    Init MouseMovingGUI

---
+++ MouseMovingGUI/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MouseMovingGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TrayContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.ContextMenuStrip = this.TrayContextMenuStrip;
+            this.TrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayIcon.Icon")));
+            this.TrayIcon.Text = "TrayIcon";
+            this.TrayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TrayIcon_MouseClick);
+            // 
+            // TrayContextMenuStrip
+            // 
+            this.TrayContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.TrayContextMenuStrip.Name = "TrayContextMenuStrip";
+            this.TrayContextMenuStrip.Size = new System.Drawing.Size(153, 70);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "开始运行";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.closeToolStripMenuItem.Text = "关闭";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TrayContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+        private System.Windows.Forms.ContextMenuStrip TrayContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ MouseMovingGUI/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseMovingGUI
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer timer = null;
+
+        private bool launched = false;
+        private bool running = false;
+
+        private int minutes = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TrayIcon.Icon = Properties.Resources.Red;
+
+
+            this.TrayIcon.Visible = true;
+            this.TrayIcon.ShowBalloonTip(1000, "MouseMoving 1.0已经启动", "我可以定时模拟用户操作鼠标的行为哦,每两分钟运行一次", ToolTipIcon.Info);
+
+            timer = new System.Timers.Timer(minutes * 60000);
+            timer.Enabled = false;
+            timer.AutoReset = true;
+            timer.Elapsed += timer_Elapsed;
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            running = true;
+            for (int i = 0; i < 4; i++)
+            {
+                ExternalWin32Ref.mouse_move(i * 1000, i * 1000);
+                Thread.Sleep(1000);
+            }
+            running = false;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            launched = false;
+            OpeateTimer();
+            System.Environment.Exit(0);
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            launched = !launched;
+            OpeateTimer();
+        }
+
+        private void TrayIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                startToolStripMenuItem.Enabled = !running;
+            }
+        }
+
+        private void OpeateTimer()
+        {
+            if (launched)
+            {
+                this.TrayIcon.Icon = Properties.Resources.Green;
+                timer.Enabled = true;
+                timer.Start();
+                startToolStripMenuItem.Text = "结束运行";
+            }
+            else
+            {
+                this.TrayIcon.Icon = Properties.Resources.Red;
+                timer.Enabled = false;
+                timer.Stop();
+                startToolStripMenuItem.Text = "开始运行";
+            }
+        }
+    }
+
+    class ExternalWin32Ref
+    {
+        [System.Runtime.InteropServices.DllImport("user32")]
+        public static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public static int mouse_move(int dx, int dy)
+        {
+            return mouse_event(MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE, dx, dy, 0, 0);
+        }
+        //移动鼠标 
+        public const int MOUSEEVENTF_MOVE = 0x0001;
+        //模拟鼠标左键按下 
+        public const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        //模拟鼠标左键抬起 
+        public const int MOUSEEVENTF_LEFTUP = 0x0004;
+        //模拟鼠标右键按下 
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        //模拟鼠标右键抬起 
+        public const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        //模拟鼠标中键按下 
+        public const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        //模拟鼠标中键抬起 
+        public const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        //标示是否采用绝对坐标 
+        public const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+    }
+}
